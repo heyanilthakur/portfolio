6a67411e5b213414010c7a800092b7da29a6b9c8
anilxthakur <anilthakur.official@hotmail.com>
2023-08-12 11:20:26 +0530

    .

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -4,7 +4,7 @@ const IconLoader = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <title>Loader Logo</title>
     <g>
-      <g id="A" transform="translate(18.000000, 15.000000)">
+      <g id="A" transform="translate(25.000000, 25.000000)">
         <path
           d="M6.07386 44H0.534091L16.5568 0.363635H22.0114L38.0341 44H32.4943L19.4545 7.26704H19.1136L6.07386 44ZM8.11932 26.9545H30.4489V31.642H8.11932V26.9545Z"
           fill="currentColor"
