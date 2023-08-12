d6357e1a9b46f02d264609e430d625738767c999
anilxthakur <anilthakur.official@hotmail.com>
2023-08-12 11:39:41 +0530

    .

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -3,15 +3,15 @@ import React from 'react';
 const IconLogo = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
     <title>Logo</title>
-    <g transform="translate(-27.000000, -22.000000)">
-      <g transform="translate(50.000000, 25.000000)">
+    <g transform="translate(-8.000000, -2.000000)">
+      <g transform="translate(11.000000, 5.000000)">
         <polygon
           id="Shape"
           stroke="currentColor"
           strokeWidth="5"
           strokeLinecap="round"
           strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
+          points="69 0 0 22 0 67 39 90 78 68 78 23"
         />
         <path
           d="M6.07386 44H0.534091L16.5568 0.363635H22.0114L38.0341 44H32.4943L19.4545 7.26704H19.1136L6.07386 44ZM8.11932 26.9545H30.4489V31.642H8.11932V26.9545Z"
