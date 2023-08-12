a6320ca3c5462106ab13bf458ba32842f9e5b780
anilxthakur <anilthakur.official@hotmail.com>
2023-08-12 11:43:06 +0530

    .

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -11,12 +11,14 @@ const IconLogo = () => (
           strokeWidth="5"
           strokeLinecap="round"
           strokeLinejoin="round"
-          points="69 0 0 22 0 67 39 90 78 68 78 23"
+          points="39 0 0 22 0 67 39 90 78 68 78 23"
         />
+        <g id="A" transform="translate(30.000000, 25.000000)">
         <path
           d="M6.07386 44H0.534091L16.5568 0.363635H22.0114L38.0341 44H32.4943L19.4545 7.26704H19.1136L6.07386 44ZM8.11932 26.9545H30.4489V31.642H8.11932V26.9545Z"
           fill="currentColor"
         />
+        </g>
       </g>
     </g>
   </svg>
